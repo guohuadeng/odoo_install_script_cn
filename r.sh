267aa3878ec1f42d28a31587bc4e58032dd0b68f
ivan deng <guohuadeng@hotmail.com>
2019-03-08 14:26:53 +0800

    opt hat

--- r.sh
+++ r.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-sudo systemctl restart postgresql.service && sudo rm /var/log/odoo/* && sudo systemctl restart odoo
+sudo systemctl restart postgresql && sudo rm /var/log/odoo/* && sudo systemctl restart odoo
+sudo systemctl status postgresql && sudo systemctl status odoo
