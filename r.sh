7894ec7f1209dafdc32fab79b0a5800226d4675c
ivan deng <guohuadeng@hotmail.com>
2021-11-11 19:59:32 +0800

    init 15 demo

--- r.sh
+++ r.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-sudo systemctl restart postgresql && sudo rm /var/log/odoo/* && sudo systemctl restart odoo
+sudo systemctl restart postgresql && sudo rm /var/log/odoo/*
+sudo systemctl stop odoo
+sudo systemctl start odoo
 sudo systemctl status postgresql && sudo systemctl status odoo
